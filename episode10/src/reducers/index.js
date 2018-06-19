f45be35edf7b0ce2cc8025a2b8c000492adf911a
Kumaran <mmurugesan@tekion.com>
2018-06-19 20:07:05 +0530

    Removed other episodes

--- episode10/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import books from './books';
-import user from './user';
-import cart from './cart';
-
-export default combineReducers({ books, user, cart })
--- episode6/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_TODO = "FETCH TODO";
-export const ADD_TODO = "ADD TODO";
-export const REMOVE_TODO = "REMOVE TODO";
-export const TOGGLE_TODO = "TOGGLE TODO";
-export const SET_VISIBILITY_FILTER = "SET VISIBILITY FILTER";
